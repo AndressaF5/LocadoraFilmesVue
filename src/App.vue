7ed6e3a78fe113c228058d02b3ac1ac41280b07e
Andressa Fernandes <andressa.fernandes96@hotmail.com>
2020-10-22 11:51:42 -0300

    Adicionando carrinho e estoque

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
         <h3 v-if="horas >= 9 && horas < 17" id="aberta">Estamos abertos</h3>
         <h3 v-else-if="horas >= 17 && horas < 18" id="quase-fechando">Estamos quase fechando</h3>
         <h3 v-else id="fechada">Estamos fechados</h3>
+
+        <p> Carrinho: {{ quantidadeNoCarrinho }} filmes </p>
       </div>
     </b-row>
       <b-row>
@@ -23,11 +25,8 @@
               {{ filme.descricao }}
             </b-card-text>
 
-            <b-card-text>
-              R$ {{ filme.valor }},00
-            </b-card-text>
-
-            <b-button href="#" block variant="dark">Alugar</b-button>
+            <b-button v-if="filme.estoqueDisponivel > 0" href="#" @click="adicionarAoCarrinho(filme)" block variant="dark">Alugar por R$ {{ filme.valor }},00</b-button>
+            <b-button v-else href="#" block variant="danger">Esgotado</b-button>
           </b-card>
         </div>
       </b-row>
@@ -35,40 +34,57 @@
 </template>
 
 <script>
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-export default {
-  name: 'App',
-  data: function(){
-        return {
-            title: "Locadora de Filmes",
-            horas: new Date().getHours(),
-            filmes : [
-              { id: 1, titulo: "Hulk", descricao: "Filme Ruim", valor: 2, imagem: "https://i.imgur.com/0uthCmp.jpg" },
-              { id: 2, titulo: "Homem de Ferro", descricao: "Homem de Ferro", valor: 10, imagem: "https://i.imgur.com/OA8pDFM.jpg" },
-              { id: 3, titulo: "Thor", descricao: "Bonito ", valor: 20, imagem: "https://i.imgur.com/mt4ZRzw.jpg" },
-              { id: 4, titulo: "Capitão América", descricao: "Um filme de capitão", valor: 40, imagem: "https://i.imgur.com/UFmSVtZ.jpg" },
-              { id: 5, titulo: "Doutor Estranho", descricao: "Magia", valor: 10, imagem: "https://i.imgur.com/pVEDruM.jpg" },
-              { id: 6, titulo: "Pantera Negra", descricao: "Um segundo filme de força", valor: 10, imagem: "https://i.imgur.com/JOSEGKf.jpg" }
-            ]
-          }
+  export default {
+    name: 'App',
+    /*components: {
+      HelloWorld,
+    }*/
+    data: function(){
+      return {
+        title: "Locadora de Filmes",
+        horas: new Date().getHours(),
+        filmes : [
+          { id: 1, titulo: "Hulk", descricao: "Filme Ruim", valor: 2, imagem: "https://i.imgur.com/0uthCmp.jpg", estoqueDisponivel: 3 },
+          { id: 2, titulo: "Homem de Ferro", descricao: "Homem de Ferro", valor: 10, imagem: "https://i.imgur.com/OA8pDFM.jpg", estoqueDisponivel: 2 },
+          { id: 3, titulo: "Thor", descricao: "Bonito ", valor: 20, imagem: "https://i.imgur.com/mt4ZRzw.jpg", estoqueDisponivel: 4 },
+          { id: 4, titulo: "Capitão América", descricao: "Um filme de capitão", valor: 40, imagem: "https://i.imgur.com/UFmSVtZ.jpg", estoqueDisponivel: 1 },
+          { id: 5, titulo: "Doutor Estranho", descricao: "Magia", valor: 10, imagem: "https://i.imgur.com/pVEDruM.jpg", estoqueDisponivel: 5 },
+          { id: 6, titulo: "Pantera Negra", descricao: "Um segundo filme de força", valor: 10, imagem: "https://i.imgur.com/JOSEGKf.jpg", estoqueDisponivel: 2 }
+        ],
+        carrinho: []
+      }
+    },
+    methods: {
+      adicionarAoCarrinho: function(filme) {
+        if(filme.estoqueDisponivel > 0){
+          this.carrinho.push(filme.id);
+          filme.estoqueDisponivel -= 1;
+        }
+      },
+      quantidadeNoCarrinhoPorFilme: function(filme){
+        var quantidade = 0;
+        for(var i = 0; i < this.carrinho.length; i++){
+          if(filme.id == this.carrinho[i]) quantidade++;
+        }
+        return quantidade;
+      }
+    },
+    computed: {
+      quantidadeNoCarrinho: function() {
+        return this.carrinho.length;
+      }
     }
-  /*components: {
-    HelloWorld,
-    LocadoraFilmes
-  }*/
-}
+
+  }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  font-family: 'Quicksand', sans-serif;
+  margin-top: 40px;
 }
 
 #locadora{
@@ -99,5 +115,9 @@ export default {
 
 .card:hover{
   transform: scale(1.05);
+}
+
+p{
+  font-size: 18px;
 }
 </style>
